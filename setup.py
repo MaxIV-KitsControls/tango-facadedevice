04c6a3c684660f03783542414c0360e172f25a60
Vincent Michel <vincent.michel@maxlab.lu.se>
2014-12-12 18:42:35 +0100

    bump version 0.1.6

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def safe_read(fname):
 
 # Setup
 setup(name = "tangods-proxydevice",
-      version = "0.1.5",
+      version = "0.1.6",
       description = "Provide a proxy device to subclass.",
       author="Vincent Michel",
       author_email="vincent.michel@maxlab.lu.se",
