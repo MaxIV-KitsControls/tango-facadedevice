e115e2d9b7422c7ca572d05ee0faa271a8d12e32
Vincent Michel <vincent.michel@maxlab.lu.se>
2015-06-16 14:34:01 +0200

    bump version 0.3.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def safe_read(fname):
 
 # Setup
 setup(name="tangods-facadedevice",
-      version="0.2.9",
+      version="0.3.0",
       description="Provide a facade device to subclass.",
       author="Vincent Michel",
       author_email="vincent.michel@maxlab.lu.se",
