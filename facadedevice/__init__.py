48ec76d712f664fd4333e2910a57a3b12c8eabc8
Vincent Michel <vincent.michel@maxlab.lu.se>
2015-12-15 18:50:25 +0100

    bring block_attribute at top level

--- facadedevice/__init__.py
+++ facadedevice/__init__.py
@@ -1,10 +1,12 @@
 """Provide a proxy device to subclass."""
 
 __all__ = ['Facade', 'FacadeMeta', 'proxy_attribute', 'local_attribute',
-           'logical_attribute', 'proxy_command', 'proxy', 'stamped']
+           'logical_attribute', 'proxy_command', 'proxy', 'block_attribute',
+           'stamped']
 
 # Imports
 from facadedevice.device import Facade, FacadeMeta
 from facadedevice.objects import local_attribute, logical_attribute
 from facadedevice.objects import proxy_attribute, proxy_command, proxy
+from facadedevice.objects import block_attribute
 from facadedevice.common import stamped
