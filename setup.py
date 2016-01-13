66a2f80c2bce57101e8eb32497631fd28ee06150
Vincent Michel <vincent.michel@maxlab.lu.se>
2016-01-13 17:17:45 +0100

    package with bdist_rpm2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def safe_read(fname):
         return ""
 
 # Setup
-setup(name="python-facadedevice",
+setup(name="facadedevice",
       version="0.6.3",
       description="Provide a facade device to subclass.",
       author="Vincent Michel",
@@ -22,5 +22,9 @@ setup(name="python-facadedevice",
       url="http://www.maxlab.lu.se",
       long_description=safe_read("README.md"),
       packages=['facadedevice'],
-      test_suite="nose.collector",
+      setup_requires=['nose', 'rpm2'],
+      install_requires=['PyTango'],
+      tests_require=['mock', 'python-devicetest'],
+      dependency_links=['git+https://github.com/vxgmichel/pytango-devicetest.git#egg=python-devicetest',
+                        'git+https://github.com/vxgmichel/setuptools-rpm2.git#egg=rpm2']
       )
