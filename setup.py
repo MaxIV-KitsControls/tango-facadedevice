154b99b6195c59f4710225d0fc3c4b9ffb101c3c
Vincent Michel <vincent.michel@maxlab.lu.se>
2016-08-15 14:50:41 +0200

    Bump version 0.8.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def safe_read(fname):
 
 # Setup
 setup(name="facadedevice",
-      version="0.7.3",
+      version="0.8.0",
       description="Provide a facade device to subclass.",
       author="Vincent Michel",
       author_email="vincent.michel@maxlab.lu.se",
