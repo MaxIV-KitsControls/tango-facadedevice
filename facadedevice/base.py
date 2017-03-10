57da45331b1798650c99682817ce6cebec7ad027
Vincent Michel <vincent.michel@maxlab.lu.se>
2017-03-10 11:50:13 +0100

    Add base module

---
+++ facadedevice/base.py
@@ -0,0 +1,232 @@
+
+import warnings
+from numpy import array_equal
+from tango import AttrQuality
+
+from functools import partial
+from collections import Mapping, namedtuple, defaultdict
+
+# Constants
+
+VALID = AttrQuality.ATTR_VALID
+INVALID = AttrQuality.ATTR_INVALID
+
+
+# Triplet object
+
+triplet = namedtuple("triplet", ("value", "stamp", "quality"))
+
+
+def compare_triplet(a, b):
+    assert isinstance(a, triplet)
+    try:
+        value, stamp, quality = b
+    except:
+        return False
+    return (a.stamp == stamp and
+            a.quality == quality and
+            array_equal(a.value, value))
+
+
+def assert_triplet(a):
+    assert isinstance(a, triplet)
+    if not isinstance(a.stamp, float):
+        raise TypeError("The timestamp is not a float")
+    if not isinstance(a.quality, int):
+        raise TypeError("The quality is not a integer")
+
+
+triplet.__new__.__defaults__ = (VALID,)
+triplet.__eq__ = compare_triplet
+triplet.__ne__ = lambda self, arg: not self.__eq__(arg)
+triplet.__init__ = lambda self, *args: assert_triplet(self)
+
+
+# Node object
+
+class Node:
+
+    def __init__(self, name, description=None, callbacks=()):
+        self._result = None
+        self._exception = None
+        self.name = name
+        self.description = description or name
+        self.callbacks = list(callbacks)
+
+    # Setters
+
+    def set_result(self, result):
+        diff = (
+            self._exception is not None or
+            self._result != result)
+        self._result = result
+        self._exception = None
+        if diff:
+            self.notify()
+
+    def set_exception(self, exception):
+        if not isinstance(exception, BaseException):
+            raise TypeError('Not a valid exception')
+        diff = (
+            self._result is not None or
+            self._exception != exception)
+        self._result = None
+        self._exception = exception
+        if diff:
+            self.notify()
+
+    # Getters
+
+    def result(self):
+        if self._exception is not None:
+            raise self._exception
+        return self._result
+
+    def exception(self):
+        return self._exception
+
+    # Notifying
+
+    def notify(self):
+        for callback in self.callbacks:
+            try:
+                callback(self)
+            except Exception as exc:
+                message = "Node {} failed to notify: {!r}"
+                warnings.warn(message.format(self.name, exc))
+
+    # Representation
+
+    def __repr__(self):
+        return "Node({!r})".format(self.name)
+
+
+# Graph object
+
+class Graph(Mapping):
+
+    def __init__(self):
+        # Graph state
+        self._nodes = {}
+        self._rules = {}
+        # Computed state
+        self._updates = {}
+        self._dependencies = defaultdict(set)
+        self._subscriptions = defaultdict(set)
+        # Propagation state
+        self._pending = set()
+        self._propagating = False
+
+    # Create graph
+
+    def add_node(self, node):
+        if node.name in self._nodes:
+            message = "A node called {} already exists"
+            raise ValueError(message.format(node.name))
+        self._nodes[node.name] = node
+
+    def add_rule(self, node, func, bind):
+        if node not in self._nodes.values():
+            message = "The node {!r} is not in the graph"
+            raise ValueError(message.format(node))
+        if node in self._rules:
+            message = "A rule for {} already exists"
+            raise ValueError(message.format(node.name))
+        self._rules[node] = func, bind
+
+    # Build dependencies
+
+    def build(self):
+        # Loop over rules
+        for node, (func, bind) in self._rules.items():
+            # Set update callbacks
+            publishers = tuple(self._nodes[name] for name in bind)
+            self._updates[node] = partial(func, *publishers)
+            # Set subscriptions
+            for publisher in publishers:
+                self._subscriptions[publisher].add(node)
+            # Compute dependencies
+            seen = set()
+            names = set(bind)
+            while names:
+                current_name = names.pop()
+                seen.add(current_name)
+                current_node = self._nodes[current_name]
+                if current_node not in self._rules:
+                    continue
+                current_rule = self._rules[current_node]
+                names |= set(current_rule[1]) - seen
+            # Check cyclic dependencies
+            if node.name in seen:
+                msg = '{} is involved in a cyclic dependency'
+                raise ValueError(msg.format(node))
+            # Set dependencies
+            self._dependencies[node] = {self._nodes[name] for name in seen}
+        # Set callbacks (after the graph is proven to be valid)
+        for publisher in self._subscriptions.keys():
+            if self.callback not in publisher.callbacks:
+                publisher.callbacks.append(self.callback)
+
+    # Reset
+
+    def reset(self):
+        # Remove callbacks
+        for publisher in self._subscriptions.keys():
+            if self.callback in publisher.callbacks:
+                publisher.callbacks.remove(self.callback)
+        # Reset computed state
+        self._updates = {}
+        self._dependencies = defaultdict(set)
+        self._subscriptions = defaultdict(set)
+        # Reset propagation state
+        self._pending = set()
+        self._propagating = False
+
+    # Propagation
+
+    def callback(self, node):
+        self._pending |= self._subscriptions[node]
+        if not self._propagating:
+            self.propagate()
+
+    def propagate(self):
+        # Set propagation flag
+        try:
+            self._propagating = True
+            # Loop over pending updates
+            while self._pending:
+                # Find next item
+                for node in self._pending:
+                    dependencies = self._dependencies[node]
+                    if self._pending.isdisjoint(dependencies):
+                        self._pending.remove(node)
+                        break
+                # Deadlock
+                else:
+                    warnings.warn('Propagation deadlocked')
+                    node = self._pending.pop()
+                # Update and notify
+                self.update(node)
+        # Reset propagation flag
+        finally:
+            self._propagating = False
+
+    def update(self, node):
+        callback = self._updates[node]
+        try:
+            result = callback()
+        except Exception as exc:
+            node.set_exception(exc)
+        else:
+            node.set_result(result)
+
+    # Dict interface
+
+    def __getitem__(self, key):
+        return self._nodes[key]
+
+    def __iter__(self):
+        return iter(self._nodes)
+
+    def __len__(self):
+        return len(self._nodes)
---
+++ tests/test_base.py
@@ -0,0 +1,300 @@
+
+import mock
+import numpy
+import pytest
+
+from facadedevice.base import Node, triplet, Graph
+from facadedevice.base import VALID, INVALID
+
+
+def test_compare_triplet():
+    a = triplet([1, 2], 0.0)
+    b = triplet(numpy.array([1, 2]), 0.0)
+    assert a == b == a == ([1, 2], 0.0, VALID) == b
+    c = triplet([1, 3], 0.0)
+    d = triplet([1, 2], 0.1)
+    e = triplet([1, 2], 0.0, INVALID)
+    f = triplet(numpy.array([1, 3]), 0.0)
+    g = None
+    assert a != c != b
+    assert a != d != b
+    assert a != e != b
+    assert a != f != b
+    assert a != g != b
+    assert a != 1 != b
+    assert a != 'test' != b
+    assert a != 'tes' != b
+
+
+def test_assert_triplet():
+    with pytest.raises(TypeError):
+        triplet([1], 'not a float')
+    with pytest.raises(TypeError):
+        triplet([1], 0.0, 'not a quality')
+
+
+def test_node_setters():
+    mocks = [mock.Mock() for _ in range(3)]
+    n = Node('test', description='desc', callbacks=mocks)
+    assert n.name == 'test'
+    assert n.description == 'desc'
+    assert repr(n) == "Node('test')"
+    assert n.exception() is None
+    assert n.result() is None
+    for m in mocks:
+        assert not m.called
+    # Set triplet
+    a = triplet([1], 0.0)
+    n.set_result(a)
+    assert n.exception() is None
+    assert n.result() == a
+    for m in mocks:
+        m.assert_called_with(n)
+        m.reset_mock()
+    # Set same triplet
+    b = triplet(numpy.array([1]), 0.0)
+    n.set_result(b)
+    assert n.exception() is None
+    assert n.result() == b
+    for m in mocks:
+        assert not m.called
+    # Set different triplet
+    c = triplet([2], 0.0)
+    n.set_result(c)
+    assert n.exception() is None
+    assert n.result() == c
+    for m in mocks:
+        m.assert_called_with(n)
+        m.reset_mock()
+    # Set exception
+    d = RuntimeError('Ooops')
+    n.set_exception(d)
+    assert n.exception() == d
+    with pytest.raises(RuntimeError):
+        n.result()
+    for m in mocks:
+        m.assert_called_with(n)
+        m.reset_mock()
+    # Set same exception
+    n.set_exception(d)
+    assert n.exception() == d
+    with pytest.raises(RuntimeError):
+        n.result()
+    for m in mocks:
+        assert not m.called
+    # Set different exception
+    e = IOError('Bim!')
+    n.set_exception(e)
+    assert n.exception() == e
+    with pytest.raises(IOError):
+        n.result()
+    for m in mocks:
+        m.assert_called_with(n)
+        m.reset_mock()
+    # Reset
+    n.set_result(None)
+    assert n.exception() is None
+    assert n.result() is None
+    for m in mocks:
+        m.assert_called_with(n)
+        m.reset_mock()
+    # Already reset
+    n.set_result(None)
+    assert n.result() is None
+    assert n.exception() is None
+    for m in mocks:
+        assert not m.called
+
+
+def test_fail_node():
+    mocks = [mock.Mock(side_effect=RuntimeError)]
+    n = Node('test', description='desc', callbacks=mocks)
+    assert n.name == 'test'
+    assert n.description == 'desc'
+    assert n.exception() is None
+    assert n.result() is None
+    for m in mocks:
+        assert not m.called
+    # Set wrong exception
+    with pytest.raises(TypeError):
+        n.set_exception(1)
+    assert n.exception() is None
+    assert n.result() is None
+    # Wrong callback
+    a = triplet(1, 0.0)
+    with pytest.warns(UserWarning) as record:
+        n.set_result(a)
+    assert n.exception() is None
+    assert n.result() == a
+    assert "RuntimeError" in record[0].message.args[0]
+
+
+def test_simple_graph():
+    a = Node('a')
+    b = Node('b')
+    g = Graph()
+    g.add_node(a)
+    g.add_node(b)
+    assert len(g) == 2
+    assert set(g) == {'a', 'b'}
+    g.add_rule(b, lambda a: a.result()*2, ['a'])
+    # Test 1
+    g.build()
+    a.set_result(2)
+    assert a.result() == 2
+    assert b.result() == 4
+    g.reset()
+    # Test 2
+    g.build()
+    a.set_result(3)
+    assert a.result() == 3
+    assert b.result() == 6
+    g.reset()
+    # Test 3
+    ma = mock.Mock()
+    mb = mock.Mock()
+    a.callbacks.append(ma)
+    b.callbacks.append(mb)
+    g.build()
+    a.set_result(4)
+    assert a.result() == 4
+    assert b.result() == 8
+    ma.assert_called_once_with(a)
+    mb.assert_called_once_with(b)
+    g.reset()
+    # Test 4
+    ma = mock.Mock()
+    mb = mock.Mock()
+    a.callbacks.append(ma)
+    b.callbacks.append(mb)
+    g.build()
+    a.set_exception(RuntimeError('Oops'))
+    with pytest.raises(RuntimeError):
+        a.result()
+    with pytest.raises(RuntimeError):
+        b.result()
+    ma.assert_called_once_with(a)
+    mb.assert_called_once_with(b)
+    g.reset()
+
+
+def test_cyclic_graph():
+    # Test 1
+    a = Node('a')
+    b = Node('b')
+    g = Graph()
+    g.add_node(a)
+    g.add_node(b)
+    g.add_rule(b, lambda a: a.result()*2, ['a'])
+    g.add_rule(a, lambda b: b.result()/2, ['b'])
+    with pytest.raises(ValueError) as context:
+        g.build()
+    assert "cyclic" in str(context.value)
+    assert a.callbacks == []
+    assert b.callbacks == []
+    # Test 2
+    a = Node('a')
+    g = Graph()
+    g.add_node(a)
+    g.add_rule(a, lambda a: a.result()*2, ['a'])
+    with pytest.raises(ValueError) as context:
+        g.build()
+    assert "cyclic" in str(context.value)
+    assert a.callbacks == []
+    # Test 3
+    a = Node('a')
+    g = Graph()
+    g.build()
+    g._dependencies[a].add(a)
+    g._subscriptions[a].add(a)
+    g._updates[a] = lambda: a.result()
+    a.callbacks.append(g.callback)
+    with pytest.warns(UserWarning) as record:
+        a.set_result(1)
+    assert "deadlock" in record[0].message.args[0]
+
+
+def test_diamond_graph():
+    # Create graph
+    graph = Graph()
+    for x in "abcdefg":
+        graph.add_node(Node(x))
+    assert len(graph) == len("abcdefg")
+    assert set(graph) == set("abcdefg")
+
+    def recover(x):
+        try:
+            return x.result()
+        except ZeroDivisionError:
+            return float('inf')
+
+    def add(x, y):
+        return x.result() + y.result()
+
+    # Set the rules
+    graph.add_rule(graph['b'], lambda a: a.result() * 10, ['a'])
+    graph.add_rule(graph['c'], lambda a: a.result() * 100, ['a'])
+    graph.add_rule(graph['d'], lambda c: c.result() // 100, ['c'])
+    graph.add_rule(graph['e'], add, ['b', 'd'])
+    graph.add_rule(graph['f'], lambda a: 1 / a.result(), ['a'])
+    graph.add_rule(graph['g'], recover, ['f'])
+
+    # Build graph
+    graph.build()
+
+    # Test 1
+    graph['a'].set_result(0)
+    for x in 'abcde':
+        print(x)
+        assert graph[x].result() == 0
+    with pytest.raises(ZeroDivisionError):
+        assert graph['f'].result()
+    assert graph['g'].result() == float('inf')
+
+    # Test 2
+    graph['a'].set_result(1)
+    assert graph['b'].result() == 10
+    assert graph['c'].result() == 100
+    assert graph['d'].result() == 1
+    assert graph['e'].result() == 11
+    assert graph['f'].result() == 1
+    assert graph['g'].result() == 1
+
+    # Test 3
+    mocks = {}
+    for x in "abcdefg":
+        mocks[x] = mock.Mock()
+        graph[x].callbacks.append(mocks[x])
+    graph['a'].set_result(2)
+    assert graph['b'].result() == 20
+    assert graph['c'].result() == 200
+    assert graph['d'].result() == 2
+    assert graph['e'].result() == 22
+    assert graph['f'].result() == 0.5
+    assert graph['g'].result() == 0.5
+    for x in "abcdefg":
+        mocks[x].assert_called_once_with(graph[x])
+
+    # Test 3
+    for x in "abcdefg":
+        mocks[x].reset_mock()
+    graph['a'].set_exception(RuntimeError('Ooops'))
+    for x in "bcdefg":
+        with pytest.raises(RuntimeError):
+            graph[x].result()
+    for x in "abcdefg":
+        mocks[x].assert_called_once_with(graph[x])
+
+
+def test_wrong_graph():
+    g = Graph()
+    g.add_node(Node('a'))
+    with pytest.raises(ValueError):
+        g.add_node(Node('a'))
+    with pytest.raises(ValueError):
+        g.add_rule(Node('b'), lambda a: a.result(), ['a'])
+    b = Node('b')
+    g.add_node(b)
+    g.add_rule(b, lambda a: a.result(), ['a'])
+    with pytest.raises(ValueError):
+        g.add_rule(b, lambda a: a.result(), ['a'])
