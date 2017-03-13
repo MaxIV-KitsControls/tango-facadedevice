4e92d976b3dbb7fd427e5777af802c64b1a29438
Vincent Michel <vincent.michel@maxlab.lu.se>
2017-03-13 18:23:44 +0100

    Update init module

--- facadedevice/__init__.py
+++ facadedevice/__init__.py
@@ -1,10 +1,11 @@
 """Provide a proxy device to subclass."""
 
 # Imports
-from facadedevice.device import Facade, FacadeMeta
+from facadedevice.device import Facade, TimedFacade
 from facadedevice.objects import local_attribute, logical_attribute
 from facadedevice.objects import proxy_attribute, proxy_command
-from facadedevice.objects import block_attribute
+from facadedevice.objects import state_attribute
 
-__all__ = ['Facade', 'FacadeMeta', 'proxy_attribute', 'local_attribute',
-           'logical_attribute', 'proxy_command', 'block_attribute']
+__all__ = ['Facade', 'TimedFacade',
+           'proxy_attribute', 'local_attribute', 'logical_attribute',
+           'state_attribute', 'proxy_command']
