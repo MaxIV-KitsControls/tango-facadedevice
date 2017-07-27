8d0dfcb2773cfb75bfc73ef03d2f5b62c19d9dc8
AntoineDupre <antoine.dupre@maxiv.lu.se>
2017-07-27 16:29:22 +0200

    Fix typo

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
         'Topic :: Software Development :: Libraries'],
 
     # Requirements
-    install_requires=['pytango>=9.2.1, numpy'],
+    install_requires=['pytango>=9.2.1', 'numpy'],
     tests_require=['pytest-mock',
                    'pytest-xdist',
                    'pytest-coverage',
